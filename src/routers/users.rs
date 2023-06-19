c15cb32e349cbb3bae5c4c98abc4c1a2ba7600e3
AsgrimS <jan.semik@gmail.com>
2023-06-19 20:39:53 +0200

    init migration

--- src/routers/users.rs
+++ src/routers/users.rs
@@ -1,8 +1,35 @@
 use axum::{http::StatusCode, routing::get, Json, Router};
 use serde::{Deserialize, Serialize};
+use std::env;
+
+use sqlx::Row;
 
 pub fn users_router() -> Router {
-    Router::new().route("/", get(|| async { "ok" }).post(create_user))
+    Router::new()
+        .route("/", get(|| async { "ok" }).post(create_user))
+        .route("/test", get(test_db))
+}
+
+struct UserDB {
+    username: String,
+}
+
+async fn test_db() -> (StatusCode, Json<i32>) {
+    let url = env::var("DATABASE_URL").expect("database URL to be in .env");
+    let pool = sqlx::postgres::PgPool::connect(url.as_str()).await.unwrap();
+    // sqlx::migrate!().run(&pool).await.unwrap();
+
+    // let res = sqlx::query_as!(UserDB, "SELECT username FROM user")
+    //     .fetch_all(&pool)
+    //     .await
+    //     .unwrap();
+
+    let res = sqlx::query("SELECT 1 + 1 as sum")
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+    let sum: i32 = res.get("sum");
+    (StatusCode::CREATED, Json(sum))
 }
 
 async fn create_user(
